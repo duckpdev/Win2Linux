32172a7ab00214b44fae1399113cb53cdfaef552
Duck.dev <94231866+duckpdev@users.noreply.github.com>
2024-08-18 17:50:41 +0300

    Version 1.2

--- win2linux.bat
+++ win2linux.bat
@@ -1,6 +1,6 @@
-
 @echo off
-
+setlocal EnableDelayedExpansion
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 cls
 
 echo Warning: All your data will be erased please make backup to another disk (very very small chance)
